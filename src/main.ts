2ee1fb38e4e4b30ce4a23414aa07e7546bd848d6
Albert Sanchez <asanchez@mobilecomputing.com.ar>
2023-01-02 17:19:45 -0300

    :bug: puerto
    fix

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
